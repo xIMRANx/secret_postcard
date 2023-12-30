58c762e222d3298b6e2ea17717f7cd0a6c44ff8d
Imran Akhmedov <xgcgdcgcgch@gmail.com>
2023-12-30 15:06:54 +0300

    Register callback router for moderation

--- app/handlers/user/__init__.py
+++ app/handlers/user/__init__.py
@@ -2,11 +2,12 @@ from aiogram import Router
 
 
 def get_user_router() -> Router:
-    from . import info, start, file
+    from . import info, start, file, callback
 
     router = Router()
     router.include_router(info.router)
     router.include_router(start.router)
     router.include_router(file.router)
+    router.include_router(callback.router)
 
     return router
